5f65a99416caa64041293ef3da5652a8d0b41602
Svantje Lilienthal <svantje.lilienthal@fu-berlin.de>
2017-10-30 18:27:37 +0100

    first commit

---
+++ SettingsForm.inc.php
@@ -0,0 +1,82 @@
+<?php
+
+/**
+ * @file plugins/generic/solr/SettingsForm.inc.php
+ *
+ * Copyright (c) 2014-2017 Simon Fraser University
+ * Copyright (c) 2003-2017 John Willinsky
+ * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ *
+ * @class SettingsForm
+ * @ingroup plugins_generic_solr
+ *
+ * @brief Form for managers to modify web feeds plugin settings
+ */
+
+import('lib.pkp.classes.form.Form');
+
+class SettingsForm extends Form {
+
+	/** @var int Associated context ID */
+	private $_contextId;
+
+	/** @var SolrPlugin */
+	private $_plugin;
+
+	/**
+	 * Constructor
+	 * @param $plugin SolrPlugin
+	 * @param $contextId int Context ID
+	 */
+	function __construct($plugin, $contextId) {
+		$this->_contextId = $contextId;
+		$this->_plugin = $plugin;
+
+		parent::__construct($plugin->getTemplatePath() . 'settingsForm.tpl');
+		$this->addCheck(new FormValidatorPost($this));
+		$this->addCheck(new FormValidatorCSRF($this));
+	}
+
+	/**
+	 * Initialize form data.
+	 */
+	function initData() {
+		$contextId = $this->_contextId;
+		$plugin = $this->_plugin;
+
+		$this->setData('searchEndpoint', $plugin->getSetting($contextId, 'searchEndpoint'));
+		$this->setData('username', $plugin->getSetting($contextId, 'username'));
+		$this->setData('password', $plugin->getSetting($contextId, 'password'));
+	}
+
+	/**
+	 * Assign form data to user-submitted data.
+	 */
+	function readInputData() {
+		$this->readUserVars(array('searchEndpoint','username','password'));
+	}
+
+	/**
+	 * Fetch the form.
+	 * @copydoc Form::fetch()
+	 */
+	function fetch($request) {
+		$templateMgr = TemplateManager::getManager($request);
+		$templateMgr->assign('pluginName', $this->_plugin->getName());
+		return parent::fetch($request);
+	}
+
+	/**
+	 * Save settings. 
+	 */
+	function execute() {
+		$plugin = $this->_plugin;
+		$contextId = $this->_contextId;
+
+		$plugin->updateSetting($contextId, 'searchEndpoint', $this->getData('searchEndpoint'));
+		$plugin->updateSetting($contextId, 'username', $this->getData('username'));
+		$plugin->updateSetting($contextId, 'password', $this->getData('password'));
+	}
+}
+
+?>
---
+++ templates/settingsForm.tpl
@@ -0,0 +1,38 @@
+{**
+ * plugins/generic/lucene/templates/settingsForm.tpl
+ *
+ * Copyright (c) 2014-2017 Simon Fraser University
+ * Copyright (c) 2003-2017 John Willinsky
+ * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ *
+ * Lucene plugin settings
+ *}
+<div id="solrSettings">
+
+<!-- <div id="description">{translate key="plugins.generic.solr.description"}</div>
+<h3>{translate key="plugins.generic.solr.settings"}</h3> -->
+
+<script>
+	$(function() {ldelim}
+		// Attach the form handler.
+		$('#solrSettingsForm').pkpHandler('$.pkp.controllers.form.AjaxFormHandler');
+	{rdelim});
+</script>
+
+<form class="pkp_form" id="solrSettingsForm" method="post" action="{url router=$smarty.const.ROUTE_COMPONENT op="manage" category="generic" plugin=$pluginName verb="settings" save=true}">
+	{csrf}
+	{include file="common/formErrors.tpl"}
+	
+	{fbvFormArea id="solrSettingsFormArea" title="plugins.generic.solr.settings.solrServerSettings"}
+
+		{fbvElement type="text" id="searchEndpoint" value=$searchEndpoint label="plugins.generic.solr.settings.searchEndpoint" size=$fbvStyles.size.MEDIUM required="true"}
+		{fbvElement type="text" id="username" value=$username label="plugins.generic.solr.settings.username" size=$fbvStyles.size.MEDIUM}
+		{fbvElement type="text" id="password" value=$password label="plugins.generic.solr.settings.password" size=$fbvStyles.size.MEDIUM}
+
+	{/fbvFormArea}
+
+	{fbvFormButtons}
+</form>
+
+<p><span class="formRequired">{translate key="common.requiredField"}</span></p>
+</div>
